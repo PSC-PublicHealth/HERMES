d7ff81dc072705ee8b30891b4342dfc4db522780
stbrown <stbrown@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2014-08-04 12:00:09 +0000

    Adding for new visualization work.
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@1800 c0c85e88-7b48-49d6-ad45-a8906df79eec

---
+++ HERMES2/src/utils/visualizationUtils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+########################################################################
+# Copyright C 2010, Pittsburgh Supercomputing Center (PSC).            #
+# =========================================================            #
+#                                                                      #
+# Permission to use, copy, and modify this software and its            #
+# documentation without fee for personal use or non-commercial use     #
+# within your organization is hereby granted, provided that the above  #
+# copyright notice is preserved in all copies and that the copyright   #
+# and this permission notice appear in supporting documentation.       #
+# Permission to redistribute this software to other organizations or   #
+# individuals is not permitted without the written permission of the   #
+# Pittsburgh Supercomputing Center.  PSC makes no representations      #
+# about the suitability of this software for any purpose.  It is       #
+# provided "as is" without express or implied warranty.                #
+#                                                                      #
+########################################################################
+
+__doc__=""" visualizationUtils.py
+This module holds miscellaneous utility functions that do not have
+a natural home elsewhere.
+"""
+
+_hermes_svn_id_="$Id: visualizationutil.py 1697 2014-04-22 21:03:16Z jleonard $"
+
+import ipath
+import math
+
+def circleLonLat(center_x,center_y,radius,resolution):
+    points = []
+    for i in range(resolution, 0, -1):
+        angle = (2 * math.pi / float(resolution)) * float(i)
+        #print str(angle) + " " + str(center_x) + " " + str(center_y) + " " + str(radius)
+        #print "Angles: " + str(math.sin(angle)) + " " + str(math.cos(angle))
+        if i == resolution:
+            orgPoint = (float(center_x) + float(radius)* math.sin(angle), float(center_y)+ float(radius) * math.cos(angle))
+        points.append((float(center_x) + radius * math.sin(angle), 
+                       float(center_y) + radius * math.cos(angle)))
+        
+    points.append((orgPoint[0],orgPoint[1]))
+     
+    return points
+
