9efee2cc528aeb8bfb30bd0866596e8c440b9905
welling <welling@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2014-09-05 21:37:36 +0000

    Unicode support for arguments to the right of the = sign in kvp files.  The identifiers to the left of = must still be latin.
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@1856 c0c85e88-7b48-49d6-ad45-a8906df79eec

--- HERMES2/src/sim/kvp_tools.py
+++ HERMES2/src/sim/kvp_tools.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf8 -*-
 
 ##########################################################################################
 # Copyright C 2009, Pittsburgh Supercomputing Center (PSC).                              #
@@ -17,7 +18,7 @@
 
 _hermes_svn_id_="$Id$"
 
-import sys,os,os.path,re,types,unittest,StringIO
+import sys,os,os.path,re,types,unittest,StringIO,chardet,codecs
 
 class TokenizerException(Exception): pass
 class ParserException(Exception): pass
@@ -56,7 +57,7 @@ class KVPParser:
         self.debug= 0
 
     @staticmethod
-    def _tokGen(text):
+    def _tokGen(text,encoding):
         """
         This returns a generator, each next() of which yields a type/token pair.
         
@@ -67,26 +68,38 @@ class KVPParser:
             m = KVPParser.tokenRe.match(text, pos)
             if not m: break
             pos = m.end()
-            if m.group('comment'): yield 'comment',m.group('comment')
-            elif m.group('identifier'): yield 'identifier',m.group('identifier')
+            if m.group('comment'): 
+                v = m.group('comment')
+                if isinstance(v,types.StringType): v = v.decode(encoding)
+                yield 'comment',v
+            elif m.group('identifier'): 
+                v = m.group('identifier')
+                if isinstance(v,types.StringType): v = v.decode(encoding)
+                yield 'identifier',v
             elif m.group('integer'): yield 'integer',int(m.group('integer'))
             elif m.group('float1'): yield 'float',float(m.group('float1'))
             elif m.group('float2'): yield 'float',float(m.group('float2'))
             elif m.group('separator'): yield 'separator',m.group('separator')
             elif m.group('equalsign'): yield 'equalsign',m.group('equalsign')
-            elif m.group('string1'): yield 'string',m.group('string1')
-            elif m.group('string2'): yield 'string',m.group('string2')
+            elif m.group('string1'): 
+                v = m.group('string1')
+                if isinstance(v,types.StringType): v = v.decode(encoding)
+                yield 'string',v
+            elif m.group('string2'): 
+                v = m.group('string2')
+                if isinstance(v,types.StringType): v = v.decode(encoding)
+                yield 'string',v
             elif m.group('trailingblanks'): yield 'trailingblanks',m.group('trailingblanks')
         if pos != len(text):
             raise TokenizerException('tokenizer stopped at pos %r of %r on <%s>' % (
                 pos, len(text),text))
 
-    def _innerParseKVP( self, iterator ):
+    def _innerParseKVP( self, iterator, encoding ):
         result= {}
         
         for rec in iterator:
             if self.debug: print 'parsing <%s>'%rec
-            tokenizer= KVPParser._tokGen(rec)
+            tokenizer= KVPParser._tokGen(rec, encoding)
             key= None
             val= None
             valType= None
@@ -167,25 +180,37 @@ class KVPParser:
                 raise ParserException("failed to lex <%s>; %s"%(rec,e))
         return result
 
-    def parse( self,iteratorOrFilename ):
+    def parse( self,iteratorOrFilename, encoding=None ):
         """
         The input can be an iterator (typically an open file or a list), or a string to
-        be interpreted as a filename.  The return value of parseKVP is a dict containing
+        be interpreted as a filename.  The optional second parameter is the encoding with which
+        to decode strings to unicode.  The return value of parseKVP is a dict containing
         keys and their associated values as defined by the file.
         """
-        if isinstance(iteratorOrFilename,types.StringType):
+        if isinstance(iteratorOrFilename,types.StringTypes):
             if self.verbose: print "parsing %s"%iteratorOrFilename
+            if encoding is None:
+                # preparse for encoding
+                with open(iteratorOrFilename,"rU") as f:
+                    lines = f.readlines()
+                    encodingInfo = chardet.detect("".join(lines))
+                    if encodingInfo['confidence'] >= 0.9: 
+                        encoding = encodingInfo['encoding']
+                    else:
+                        encoding = sys.getdefaultencoding()
+                
             with open(iteratorOrFilename,"rU") as f:
-                result= self._innerParseKVP(f)
+                result= self._innerParseKVP(f,encoding)
         else:
             if self.verbose: print "parsing kvp input"
-            result= self._innerParseKVP(iter(iteratorOrFilename))
+            if encoding is None: encoding = sys.getdefaultencoding()
+            result= self._innerParseKVP(iter(iteratorOrFilename), encoding)
         
         return result
 
     def _innerWriteKVP(self,ofile,dict):
         for k,v in dict.items():
-            if isinstance(v,types.StringType):
+            if isinstance(v,types.StringTypes):
                 if '"' in v: 
                     ofile.write("%s = '%s'\n"%(k,v)) # try to preserve the quoted substring in v
                 else:
@@ -196,7 +221,7 @@ class KVPParser:
                 else:
                     vstr= ""
                     for e in v:
-                        if isinstance(e,types.StringType):
+                        if isinstance(e,types.StringTypes):
                             if '"' in e:
                                 vstr += "'%s',"%e
                             else:
@@ -208,7 +233,7 @@ class KVPParser:
                 ofile.write('%s = %s\n'%(k,v))
                 
     
-    def writeKVP( self, ofileOrFilename, dict ):
+    def writeKVP( self, ofileOrFilename, dict, encoding=None ):
         """
         Write the dictionary contents to the given open file as a key-value pair table.
         If ofileOrFilename is a string, that string will be treated as the path to the
@@ -216,11 +241,21 @@ class KVPParser:
         """
         if isinstance(ofileOrFilename,types.StringType):
             if self.verbose: print "writing to %s"%ofileOrFilename
-            with open(ofileOrFilename,"w") as f:
+            with open(ofileOrFilename,"w") as rawFile:
+                if encoding is None:
+                    if hasattr(rawFile,'encoding'): encoding = rawFile.encoding
+                    else: encoding = 'utf8'
+                f = codecs.getwriter(encoding)(rawFile, 'replace')
                 self._innerWriteKVP(f,dict)
         else:
             if self.verbose: print "writing to %s"%ofileOrFilename.name
-            self._innerWriteKVP(ofileOrFilename,dict)
+            if encoding is None:
+                if hasattr(ofileOrFilename,'encoding'):
+                    encoding = ofileOrFilename.encoding
+                else:
+                    encoding = 'utf8'
+            f = codecs.getwriter(encoding)(ofileOrFilename, 'replace')
+            self._innerWriteKVP(f,dict)
     
 
 def main():
@@ -237,7 +272,7 @@ def main():
         # Run some internal tests
         for rec in [a for a,b in TestKVPTools.samples]:
             try:
-                d= parser.parse([rec]) 
+                d= parser.parse([rec],encoding='utf8') 
                 print d  
             except ParserException,e:
                 print e
@@ -254,7 +289,8 @@ def main():
         assert(testDict==d)
 
 class TestKVPTools(unittest.TestCase):
-    samples= [("# This is a pure comment",{}),
+    samples= [
+              ("# This is a pure comment",{}),
               ("    # so is this   ",{}),
               ("foo",{'foo':True}),
               ("bar # my comment",{'bar':True}),
@@ -276,7 +312,8 @@ class TestKVPTools(unittest.TestCase):
               ("thing13='quoted string with an 0.8 embedded float'",{'thing13': 'quoted string with an 0.8 embedded float'}),
               ("somebool= True",{'somebool': True}),
               ("somebool= false",{'somebool': False}),
-              ("something= None",{'something': None})
+              ("something= None",{'something': None}),
+              ('utf8sample= "Dépôt Central*"',{'utf8sample':u'Dépôt Central*'}),
               ]
     
     def test_kvptools(self):
@@ -284,7 +321,7 @@ class TestKVPTools(unittest.TestCase):
         totalDict = {}
         for rec,expectedResult in TestKVPTools.samples:
             try:
-                d= parser.parse([rec])
+                d= parser.parse([rec],encoding='utf8')
                 self.assertTrue(isinstance(expectedResult, types.DictType))
                 if d != expectedResult:
                     print "error: got %s expected %s"%(d,expectedResult)
@@ -294,13 +331,13 @@ class TestKVPTools(unittest.TestCase):
                 self.assertTrue(isinstance(expectedResult, types.StringTypes))
                 self.assertTrue(str(e)==expectedResult)
         outSIO = StringIO.StringIO()
-        parser.writeKVP(outSIO,totalDict)
+        parser.writeKVP(outSIO,totalDict,encoding='utf8')
         inSIO = StringIO.StringIO(outSIO.getvalue())
-        testDict= parser.parse(inSIO)
+        testDict= parser.parse(inSIO,encoding='utf8')
         self.assertTrue(testDict == totalDict)
-#        for k,v in testDict.items():
-#            #print "<%s>:<%s>"%(k,v)
-#            self.assertTrue(k in totalDict and totalDict[k]==v)
+        for k,v in testDict.items():
+            #print "<%s>:<%s>"%(k,v)
+            self.assertTrue(k in totalDict and totalDict[k]==v)
             
 ############
 # Main hook
