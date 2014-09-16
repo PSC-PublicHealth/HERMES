2bf0901afe33e240d8ab864d94a245b17015d034
depasse <depasse@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2014-09-16 21:56:40 +0000

    use to set environment variables in bash
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@1889 c0c85e88-7b48-49d6-ad45-a8906df79eec

---
+++ HERMES2/src/setpaths.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Everything is reletive to the HERMES2 top-level directory.
+# Assume that this script is in HERMES2/src
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+THISDIR=`dirname $THISDIR`
+
+export HERMES_BASE=$THISDIR
+
+HERMES_DATA_PATH=""
+
+for d in $HERMES_BASE/master_data/*; do echo `basename $d` / `basename $d/*.kvp`; HERMES_DATA_PATH=$d:$HERMES_DATA_PATH; done;
+
+export HERMES_DATA_PATH
+
+alias hermes=$HERMES_BASE/src/sim/main.py
+
