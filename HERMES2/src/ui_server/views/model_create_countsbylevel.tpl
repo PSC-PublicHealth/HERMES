PSC-PublicHealth/HERMES

commit 8ecf2509b9ad3ff9bf61b44a121fa8d6b35ff1c2

Added an Expert button to Model Create pg.2-5 to go to Editor
git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@1896 c0c85e88-7b48-49d6-ad45-a8906df79eec

HERMES2/src/ui_server/views/model_create_countsbylevel.tpl
@@ -36,6 +36,7 @@
       <tr>
         <td width 10%><input type="button" id="back_button" value={{_("Back")}}></td>
         <td></td>
+        <td width=10%><input type="button" id="expert_button" value="Expert"></td>
         <td width=10%><input type="button" id="next_button" value={{_("Next")}}></td>
       </tr>
     </table>
@@ -65,11 +66,8 @@ $(function() {
         }
 	});
 
-	var btn = $("#next_button");
-	btn.button();
-	btn.click( function() {
+  function validate_inputs() {
 		var parms = "";
-		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels}}; i++) {
 		   var s = "model_create_lcounts_"+(i+1);
@@ -84,10 +82,29 @@ $(function() {
 		       }
 		   }
 		   else {
-		       valsOK = false;
+		       parms = null;
 		   }
 		}
-		if (valsOK) {
+		return parms;
+	}
+
+  var btn = $("#expert_button");
+  btn.button();
+	btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
+			window.location = "{{rootPath}}model-create/next?"+parms+"&expert=true";
+		}
+		else {
+			$("#dialog-modal").dialog("open");
+		}
+	});
+
+	var btn = $("#next_button");
+	btn.button();
+	btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
 			window.location = "{{rootPath}}model-create/next?"+parms;
 		}
 		else {

HERMES2/src/ui_server/views/model_create_interlevel.tpl
@@ -68,6 +68,7 @@
       <tr>
         <td width 10%><input type="button" id="back_button" value={{_("Back")}}></td>
         <td></td>
+        <td width=10%><input type="button" id="expert_button" value={{_("Expert")}}></td>
         <td width=10%><input type="button" id="next_button" value={{_("Next")}}></td>
       </tr>
     </table>
@@ -97,11 +98,8 @@ $(function() {
         }
 	});
 
-	var btn = $("#next_button");
-	btn.button();
-	btn.click( function() {
+  function validate_inputs() {
 		var parms = "";
-		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels-1}}; i++) {
 		    var s = "model_create_interl_isfetch_"+(i+2);
@@ -116,7 +114,7 @@ $(function() {
 		        }
 		    }
 		    else {
-		        valsOK = false;
+		        parms = null;
 		    }
 		    s = "model_create_interl_issched_"+(i+2);
 		    sval = $("#"+s).val();
@@ -124,7 +122,7 @@ $(function() {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        valsOK = false;
+		        parms = null;
 		    }
 		    s = "model_create_interl_howoften_"+(i+2);
 		    sval = $("#"+s).val();
@@ -132,7 +130,7 @@ $(function() {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        valsOK = false;
+		        parms = null;
 		    }
 		    s = "model_create_interl_ymw_"+(i+2);
 		    sval = $("#"+s).val();
@@ -140,10 +138,29 @@ $(function() {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        valsOK = false;
+		        parms = null;
 		    }
 		}
-		if (valsOK) {
+		return parms;
+	}
+
+	var btn = $("#expert_button");
+	btn.button();
+	btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
+			window.location = "{{rootPath}}model-create/next?"+parms+"&expert=true";
+		}
+		else {
+			$("#dialog-modal").dialog("open");
+		}
+	});
+
+	var btn = $("#next_button");
+	btn.button();
+	btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
 			window.location = "{{rootPath}}model-create/next?"+parms;
 		}
 		else {

HERMES2/src/ui_server/views/model_create_interlevel_timings.tpl
@@ -30,6 +30,7 @@
       <tr>
         <td width 10%><input type="button" id="back_button" value={{_("Back")}}></td>
         <td></td>
+        <td width=10%><input type="button" id="expert_button" value={{_("Expert")}}></td>
         <td width=10%><input type="button" id="next_button" value={{_("Next")}}></td>
       </tr>
     </table>
@@ -70,6 +71,35 @@ $(function() {
 				$( this ).dialog( "close" );
         	}
         }
+	});
+
+	var btn = $("#expert_button");
+	btn.button();
+	btn.click( function() {
+		var dict = {};
+		$("#model_create_timing_form input,select").each( function( index ) {
+			var tj = $(this);
+			dict[tj.attr('id')] = tj.val();
+		});
+		$.getJSON("{{rootPath}}json/model-create-timing-verify-commit",dict)
+		.done(function(data) {
+			if (data.success) {
+				if (data.value) {
+					window.location = "next?expert=true";
+				}
+				else {
+					$("#dialog-modal").text(data['msg']);
+					$("#dialog-modal").dialog("open");
+				}
+			}
+			else {
+				alert('{{_("Failed: ")}}'+data.msg);
+			}
+    	})
+  		.fail(function(jqxhr, textStatus, error) {
+  			alert("Error: "+jqxhr.responseText);
+		});
+		
 	});
 
 	var btn = $("#next_button");

HERMES2/src/ui_server/views/model_create_levelnames.tpl
@@ -27,6 +27,7 @@
       <tr>
         <td width 10%><input type="button" id="back_button" value="Back"></td>
         <td></td>
+        <td width=10%><input type="button" id="expert_button" value="Expert"></td>
         <td width=10%><input type="button" id="next_button" value="Next"></td>
       </tr>
     </table>
@@ -56,11 +57,8 @@ $(function() {
         }
 	});
 
-	var btn = $("#next_button");
-	btn.button();
-	btn.click( function() {
+function validate_inputs() {
 		var parms = "";
-		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels}}; i++) {
 		   var s = "model_create_levelname_"+(i+1);
@@ -75,10 +73,29 @@ $(function() {
 		       }
 		   }
 		   else {
-		       valsOK = false;
+		       parms = null;
 		   }
 		}
-		if (valsOK) {
+		return parms;
+  }
+
+  var btn = $("#expert_button");
+  btn.button();
+  btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
+			window.location = "{{rootPath}}model-create/next?"+parms+"&expert=true";
+		}
+		else {
+			$("#dialog-modal").dialog("open");
+		}
+	})
+  
+	var btn = $("#next_button");
+	btn.button();
+	btn.click( function() {
+		var parms = validate_inputs();
+		if (parms != null) {
 			window.location = "{{rootPath}}model-create/next?"+parms;
 		}
 		else {

HERMES2/src/ui_server/modelhooks.py
@@ -628,6 +628,27 @@ def modelCreatePage(db,uiSession,step="unknown"):
                     pass
                 else:
                     newModelInfo['name'],newModelInfo['modelId'] = _createModel(db,uiSession)
+        if 'expert' in bottle.request.params and bottle.request.params['expert'] == 'true':
+                if 'modelId' in newModelInfo:
+                    # the user is messing around with the back/next buttons
+                    pass
+                else:
+                    #fake the remaining unfilled pages so the PreOrderTree can get created
+                    if 'levelcounts' not in newModelInfo: newModelInfo['levelcounts'] = [1 for x in xrange(newModelInfo['nlevels'])]
+                    if 'shippatterns_transitunits' not in newModelInfo: newModelInfo['shippatterns_transitunits'] = ['hour' for x in xrange(newModelInfo['nlevels']-1)]
+                    if 'shippatterns_transittimes' not in newModelInfo: newModelInfo['shippatterns_transittimes'] = [1.0 for x in xrange(newModelInfo['nlevels']-1)]
+                    if 'shippatterns' not in newModelInfo: newModelInfo['shippatterns'] = [() if x==0 else (False, True, 1, 'year') for x in xrange(newModelInfo['nlevels'])]
+                    if 'pottable' not in newModelInfo:
+                        pot,recDict = _buildPreOrderTree(newModelInfo)
+                        newModelInfo['pottable'] = pot.table
+                        newModelInfo['potrecdict'] = recDict
+                        newModelInfo['maxidcode'] = NumberedNameGenerator.totCount
+                    newModelInfo['name'],newModelInfo['modelId'] = _createModel(db,uiSession)
+                    p = bottle.request.path
+                    fp = bottle.request.fullpath
+                    offset = fp.find(p)
+                    rootPath = fp[:offset+1] # to include slash
+                    bottle.redirect('{}model-edit-structure?id={}'.format(rootPath,newModelInfo['modelId']))
         if 'provision' in bottle.request.params and bottle.request.params['provision'] == 'true':
             if 'modelId' in newModelInfo:
                 _provisionModel(db, newModelInfo['modelId'])

commit 87732af07d75944ad77f0143495700403c6ecac4

Pre-populate how often goods are shipped b/w levels, fix form validation I broke in r1896, add mousewheel support to Shipping and Times model-create pages
git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@1932 c0c85e88-7b48-49d6-ad45-a8906df79eec

HERMES2/src/ui_server/views/model_create_countsbylevel.tpl
@@ -86,6 +86,7 @@ $(function() {
 
   function validate_inputs() {
 		var parms = "";
+		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels}}; i++) {
 		   var s = "model_create_lcounts_"+(i+1);
@@ -100,9 +101,10 @@ $(function() {
 		       }
 		   }
 		   else {
-		       parms = null;
+		       valsOK = false;
 		   }
 		}
+		if (!valsOK) { parms = null; }
 		return parms;
 	}
 

HERMES2/src/ui_server/views/model_create_interlevel.tpl
@@ -30,7 +30,7 @@
 %            else:
   	        <option value="true">{{_('On A Fixed Schedule')}}</option><option value="false" selected>{{_('As Needed')}}</option></select></td>
 %            end
-  	    <td><input type="number" id="model_create_interl_howoften_{{i}}" value={{howoften}}></td>
+  	    <td><input type="number" id="model_create_interl_howoften_{{i}}" value={{howoften}} min=1></td>
   	    <td>{{_("times per")}}</td>
   	    <td><select id="model_create_interl_ymw_{{i}}">
 %            if ymw == 'year':
@@ -53,7 +53,14 @@
   	    </select></td>
   	    <td><select id="model_create_interl_issched_{{i+2}}">
   	        <option value="true">{{_('On A Fixed Schedule')}}</option><option value="false">{{_('As Needed')}}</option></select></td>
-  	    <td><input type="number" id="model_create_interl_howoften_{{i+2}}" value=1></td>
+%           prepopval = {3: [4], 4: [2,4], 5: [2,4,6]}
+%           try:
+%               val = prepopval[nlevels][i]
+%           except:
+%               val = 1
+%           end
+%           if i==(nlevels-2): val = 12
+  	    <td><input type="number" id="model_create_interl_howoften_{{i+2}}" value="{{val}}" min=1></td>
   	    <td>{{_("times per")}}</td>
   	    <td><select id="model_create_interl_ymw_{{i+2}}">
   	        <option value="year">{{_('Year')}}</option><option value="month">{{_('Month')}}</option><option value="week">{{_('Week')}}</option>
@@ -79,6 +86,23 @@
 </div>
 
 <script>
+$(':input[type=number]').bind('mousewheel DOMMouseScroll', function(event){
+  var val = parseInt(this.value);
+  var maxattr = $(this).attr("max");
+  var minattr = $(this).attr("min");
+  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+    if (typeof maxattr == typeof undefined || maxattr == false || val < maxattr) {
+      this.value = val + 1;
+    }
+  }
+  else {
+    if (typeof minattr == typeof undefined || minattr == false || val > minattr) {
+      this.value = val - 1;
+    }
+  }
+  event.preventDefault(); //to prevent the window from scrolling
+});
+
 $(function() {
 	var btn = $("#back_button");
 	btn.button();
@@ -100,6 +124,7 @@ $(function() {
 
   function validate_inputs() {
 		var parms = "";
+		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels-1}}; i++) {
 		    var s = "model_create_interl_isfetch_"+(i+2);
@@ -114,7 +139,7 @@ $(function() {
 		        }
 		    }
 		    else {
-		        parms = null;
+		        valsOK = false;
 		    }
 		    s = "model_create_interl_issched_"+(i+2);
 		    sval = $("#"+s).val();
@@ -122,15 +147,15 @@ $(function() {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        parms = null;
+		        valsOK = false;
 		    }
 		    s = "model_create_interl_howoften_"+(i+2);
 		    sval = $("#"+s).val();
- 		    if (sval) {
+ 		    if (sval && parseInt(sval) > 0) {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        parms = null;
+		        valsOK = false;
 		    }
 		    s = "model_create_interl_ymw_"+(i+2);
 		    sval = $("#"+s).val();
@@ -138,9 +163,10 @@ $(function() {
 		        parms = parms + "&" + s + "=" + sval;
 		    }
 		    else {
-		        parms = null;
+		        valsOK = false;
 		    }
 		}
+		if (!valsOK) { parms = null; }
 		return parms;
 	}
 

HERMES2/src/ui_server/views/model_create_interlevel_timings.tpl
@@ -13,7 +13,7 @@
 		<td>{{_('to')}}</td>
 		<td><b>{{client}}</b></td>
 		<td>{{_('takes')}}</td>
-		<td><input type="number" id="model_create_timing_{{count}}" name="model_create_timing_{{count}}"></td>
+		<td><input type="number" id="model_create_timing_{{count}}" name="model_create_timing_{{count}}" min=1></td>
 		<td><select id="model_create_timing_units_{{count}}" name="model_create_timing_units_{{count}}">
   	        <option value="year">{{_('Years')}}</option>
   	        <option value="month">{{_('Months')}}</option>
@@ -38,6 +38,23 @@
 <div id="dialog-model"></div>
 
 <script>
+$(':input[type=number]').bind('mousewheel DOMMouseScroll', function(event){
+  var val = parseInt(this.value);
+  var maxattr = $(this).attr("max");
+  var minattr = $(this).attr("min");
+  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+    if (typeof maxattr == typeof undefined || maxattr == false || val < maxattr) {
+      this.value = val + 1;
+    }
+  }
+  else {
+    if (typeof minattr == typeof undefined || minattr == false || val > minattr) {
+      this.value = val - 1;
+    }
+  }
+  event.preventDefault(); //to prevent the window from scrolling
+});
+
 $(function() {
 	var btn = $("#back_button");
 	btn.button();

HERMES2/src/ui_server/views/model_create_levelnames.tpl
@@ -17,22 +17,14 @@
 %        for i in xrange(nlevels):
 	  	<tr>
   			<td><label for="model_create_levelname_{{i+1}}">{{_("Name for level")}} {{i+1}}</label></td>
-%           val = _("Level") + " " + str(i+1)
-%           if i==0: val = _("Central")
-%           if nlevels==3:
-%               if i==1: val = _("District")
-%           end
-%           #elif didn't work here for some reason
-%           if nlevels==4:
-%               if i==1: val = _("Region")
-%               if i==2: val = _("District")
-%           end
-%           if nlevels==5:
-%               if i==1: val = _("Province")
-%               if i==2: val = _("Region")
-%               if i==3: val = _("District")
+%           prepopval = {3: {1:_("District")}, 4: {1:_("Region"),2:_("District")}, 5: {1:_("Province"),2:_("Region"),3:_("District")}}
+%           try:
+%               val = prepopval[nlevels][i]
+%           except:
+%               val = _("Level") + " " + str(i+1)
 %           end
 %           if i==(nlevels-1): val = _("Health Post")
+%           if i==0: val = _("Central")
   			<td><input type="text" name="model_create_levelname_{{i+1}}" id="model_create_levelname_{{i+1}}" value="{{val}}"></td>
   		</tr>
 %        end
@@ -75,6 +67,7 @@ $(function() {
 
 function validate_inputs() {
 		var parms = "";
+		var valsOK = true;
 		var first = true;
 		for (var i=0; i<{{nlevels}}; i++) {
 		   var s = "model_create_levelname_"+(i+1);
@@ -89,9 +82,10 @@ function validate_inputs() {
 		       }
 		   }
 		   else {
-		       parms = null;
+		       valsOK = false;
 		   }
 		}
+		if (!valsOK) { parms = null; }
 		return parms;
   }
 