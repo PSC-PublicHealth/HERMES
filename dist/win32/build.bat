f70b9faf436312229a604f8db7b9029ac0b939ca
ez <ez@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2014-11-14 17:24:33 +0000

    Caused installer build.bat to use included 32-bit /python interpreter. Removed virtualenv dependency.
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@2007 c0c85e88-7b48-49d6-ad45-a8906df79eec

--- dist/win32/build.bat
+++ dist/win32/build.bat
@@ -1,6 +1,4 @@
 @echo off
-python -m virtualenv --help>nul
-if %errorlevel%==1 goto :novenv
 setlocal
 set oldcurdir=%cd%
 cd /d %~dp0
@@ -10,25 +8,12 @@ if exist requirements\nul (
     echo Requirements already downloaded, not updating
     goto :compile
 )
-python -m virtualenv requirements
-requirements\Scripts\pip install -r ..\..\HERMES2\requirements.txt
-echo Adding 32-bit .exe to \requirements\scripts
-setlocal EnableDelayedExpansion 
-cd /d %~dp0\requirements\Scripts
-for %%f in (*-script.py) do (
-    set filename=%%~nf
-    copy /y ..\Lib\site-packages\setuptools\cli-32.exe !filename:~0,-7!.exe
-)
-setlocal DisableDelayedExpansion
-cd /d %~dp0
-echo Merging \python into \requirements
-xcopy python requirements /e /s /y /q
-echo Installing 32-bit pip to \requirements
-requirements\python -m pip install --upgrade --force-reinstall pip
+echo Copying /python to /requirements
+xcopy python requirements\ /e /s /q
+requirements\python.exe ..\tools\get-pip.py
+requirements\python.exe -m pip install -r ..\..\HERMES2\requirements.txt
 :compile
 ..\tools\InnoSetup\iscc /qp /O"build" herm-win32-setup.iss
 cd /d %oldcurdir%
 endlocal
 goto :EOF
-:novenv
-echo Error: virtualenv not installed
