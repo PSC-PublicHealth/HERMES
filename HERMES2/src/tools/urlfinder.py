e0d649f33804e4caf8d044913f969283bd3f5f44
welling <welling@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2015-01-07 19:55:20 +0000

    A tool to help find urls in .tpl files.
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@2160 c0c85e88-7b48-49d6-ad45-a8906df79eec

---
+++ HERMES2/src/tools/urlfinder.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+"""
+This program takes a directory as its single argument.  First it traverses downward 
+from the given directory, searching all .py files looking for lines beginning with 
+@bottle.route, which define urls.  It collects all the urls and produces regular
+expressions to match them.
+
+It then re-traverses downward from the same starting directory, looking for files
+of type .tpl .  For each, it finds lines that appear to contain one of the urls found
+in the first pass and outputs those lines.
+"""
+
+import re, sys, os, os.path
+
+routeRegex = re.compile('^\s*@bottle.route\(')
+urlStripRegex = re.compile(r'^\s*@bottle.route\(\s*((\'(?P<url1>[^\']+)\')|("(?P<url2>[^"]+)"))')
+
+
+def getURL(line):
+    """Strip out the URL part of a @bottle.route line"""
+    m = urlStripRegex.match(line)
+    if m:
+        if m.group('url1'):
+            return m.group('url1')
+        else:
+            return m.group('url2')
+    else:
+        return None
+
+
+def cleanURL(url):
+    assert url.startswith('/'), 'Found an un-rooted URL: %s' % url
+    url = url[1:]
+    parts = url.split('/')
+    for offset, p in enumerate(parts):
+        if p.startswith(':') or (p.startswith('<') and p.endswith('>')):
+            parts = parts[:offset]
+            break
+    return '/'.join(parts)
+
+def removeDups(urlList):
+    l = urlList[:]
+    l.sort()
+    noDupsList = []
+    prev = None
+    for url in l:
+        if url != prev:
+            noDupsList.append(url)
+        prev = url
+    return noDupsList
+
+urlList = []
+startingDir = sys.argv[1]
+for root, dirs, files in os.walk(startingDir):
+    for fname in files:
+        if fname.endswith('.py'):
+            with open(os.path.join(root, fname), 'rU') as f:
+                for line in f.readlines():
+                    if routeRegex.match(line.strip()):
+                        urlList.append(cleanURL(getURL(line)))
+
+urlList = removeDups(urlList)
+reList = [(url, re.compile(url)) for url in urlList]
+
+for root, dirs, files in os.walk(startingDir):
+    for fname in files:
+        if fname.endswith('.tpl'):
+            fullName = os.path.join(root, fname)
+            with open(fullName, 'rU') as f:
+                for offset, line in enumerate(f.readlines()):
+                    for url, regex in reList:
+                        if regex.search(line):
+                            # print 'matches %s :' % url
+                            print '%s(%s): %s' % (fullName, offset+1, line.strip())
+
