a0c4d09e6a3cae2255aad7ac41796943bf879cde
ez <ez@c0c85e88-7b48-49d6-ad45-a8906df79eec>
2015-02-26 16:47:30 +0000

    Add full-size icon into Windows tray icon launcher
    git-svn-id: https://redmine.hindsight.psc.edu/svn/hermes/trunk@2284 c0c85e88-7b48-49d6-ad45-a8906df79eec

--- dist/win32/misc/build-hermes-tray.bat
+++ dist/win32/misc/build-hermes-tray.bat
@@ -2,6 +2,6 @@
 setlocal
 set oldcurdir=%cd%
 cd /d %~dp0
-..\..\tools\Ahk2exe\Ahk2exe.exe /in "hermes-tray.ahk" /icon "..\..\..\HERMES2\src\ui_www\icons\favicon.ico"
+..\..\tools\Ahk2exe\Ahk2exe.exe /in "hermes-tray.ahk" /icon "win.ico"
 cd /d %oldcurdir%
 endlocal
