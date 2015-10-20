21d677e959f2f369743dcffe015b6f82f5d6a63e
jleonard <jleonard@psc.edu>
2015-10-20 12:50:46 -0400

    updates to share3 code and test routines.

--- HERMES2/src/tools/test_calc_fill.py
+++ HERMES2/src/tools/test_calc_fill.py
@@ -21,7 +21,7 @@ This tool provides test routines for warehouse.share3_calculateOwnerStorageFillR
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.cm as cm
 import ipath
 
 import warehouse
@@ -44,12 +44,16 @@ import input
 import storagemodel
 import packagingmodel
 import util
+from math import pi
+import random
+
 #globals.deterministic = True
 
 warehouse.DEBUG = True
 
 
-def buildMockTypeManager(userInput, sim):
+def buildMockTypeManager(userInput, sim, vacList=None, 
+                         coldBoxSizes={'freezer':5.0,'cooler':5.0,'roomtemperature':5.0}):
     verbose = debug = False
     typeManager= typemanager.TypeManager([], verbose=verbose, debug=debug, sim=sim)
     trackableTypes = trackabletypes.TrackableTypeManager(typeManager)
@@ -98,13 +102,19 @@ def buildMockTypeManager(userInput, sim):
                       "LifetimeOpenDays":0.25,
                       "RandomKey":1000},], 
                     vaccinetypes.VaccineType,vaccinetypes.DeliverableVaccineType)]
+    
+    if vacList is not None:
+        sourceList += [(vacList, vaccinetypes.VaccineType,vaccinetypes.DeliverableVaccineType)]
 
 
+    cbs = coldBoxSizes
     sourceList += [([{"Make":"mothernature", "Model": "outdoors", "Year":"U", "Energy":"U",
                       "roomtemperature":1000000000, "Name":"OUTDOORS",
                       "ClassName":"Fridge"},
                      {"Make":"mk", "Model":"mod", "Year":"2000", "Energy":"", 
-                      "freezer":5.0, "cooler":3.0, "roomtemperature":6.0,
+                      "freezer":cbs['freezer'], 
+                      "cooler":cbs['cooler'], 
+                      "roomtemperature":cbs['roomtemperature'],
                       "Name":"coldbox_1", "ClassName":"ShippableFridge"},],
                     None, fridgetypes.FridgeType)]
 
@@ -133,10 +143,10 @@ class _mockSim:
     """
     This is used to mock the sim parameter needed by DemandModels during testing.
     """
-    def __init__(self):
+    def __init__(self, vacList=None, coldBoxSizes={'freezer':5.0,'cooler':5.0,'roomtemperature':5.0}):
         self.userInput = input.UserInput()
         self.userInput['limitedroomtemp'] = True
-        self.typeManager, self.typeManagers = buildMockTypeManager(self.userInput, self)
+        self.typeManager, self.typeManagers = buildMockTypeManager(self.userInput, self, vacList, coldBoxSizes)
 
         for k,v in self.typeManagers.items():
             setattr(self, k, v)
@@ -207,6 +217,32 @@ def calcAvailableVolumes(truck, vc):
     return fAvail, cAvail, wAvail
 
 
+def calcStats(truck, vcList, vcReq):
+    facRate_score = 0
+    for vc, facAttr in zip(vcList, ('freezerFac','coolerFac','roomtempFac')):
+        totVol = 0.0
+        for v,r in vc.items():
+            req = vcReq[v]
+            volPerVial = v.dosesPerVial * v.ccPerDose
+            vials = r * req
+            totVol += volPerVial * vials
+        
+
+        for v,r in vc.items():
+            req = vcReq[v]
+            volPerVial = v.dosesPerVial * v.ccPerDose
+            vials = r * req
+            vol = volPerVial * vials
+            fac = getattr(v, facAttr)
+
+            facRate_score += fac * r
+
+    scores = {'facRate': facRate_score}
+    return scores
+
+    
+
+
 def calcVolumesUsed(truck, sM, fVC, cVC, wVC):
     fTot = 0
     cTot = 0
@@ -261,6 +297,305 @@ def sharesToFracs(shares, totAvail, labels):
         labels += ['Empty']
     return fracs, labels
 
+def makeVacRec(name, ltf=200.0, ltc=200.0, ltr=200.0, vol=1.0, dpv=1, other={}):
+    vr = {"Name":name,
+          "DisplayName":name,
+          "LifetimeFreezerDays":ltf,
+          "LifetimeCoolerDays":ltc,
+          "LifetimeRoomTempDays":ltr,
+          "Packed vol/dose(cc) of vaccine":vol,
+          "Packed vol/dose(cc) of diluent":vol,
+          "Doses per vial":dpv,
+          "LifetimeOpenDays":0.25,
+          "Doses/person":1,
+          "RandomKey":1000}
+
+    for k,v in other.items():
+        vr[k] = v
+
+    return vr
+
+def rDur(flags, which):
+    "generate a random lifetime or an almost 0 based on a flag"
+    if flags & 1<<which:
+        return random.uniform(50.0,400.0)
+    return 0.04
+
+def rDurs():
+    """
+    generate a set of three lifetimes for a good, where at least one 
+    lifetime is long enough to survive some time in a supply chain
+    """
+    f = random.randint(1,7)
+    return (rDur(f,0), rDur(f,1), rDur(f,2))
+
+def calcVols(vcCt, vcRat):
+    vol = 0.0
+    for v,r in vcRat.items():
+        n = vcCt[v] * r
+        volPerVial = v.dosesPerVial * v.ccPerDose
+        vol += volPerVial * n
+    return vol
+
+def metrics(vc, fVc, cVc, wVc):
+    pass
+
+
+def chartCollection(vcList, vcReq, axList):
+    for vc, ax, facAttr in zip(vcList, axList, ('freezerFac','coolerFac','roomtempFac')):
+        totVol = 0.0
+        for v,r in vc.items():
+            req = vcReq[v]
+            volPerVial = v.dosesPerVial * v.ccPerDose
+            vials = r * req
+            totVol += volPerVial * vials
+        
+        theta = []
+        volWid = []
+        color = []
+        height = []
+
+        for v,r in vc.items():
+            color.append(float(v.name[1])/10.0)
+            req = vcReq[v]
+            volPerVial = v.dosesPerVial * v.ccPerDose
+            vials = r * req
+            vol = volPerVial * vials
+            volWid.append(vol/totVol*2*pi)
+            height.append(1/getattr(v, facAttr))
+        curT = 0.0
+        for vw in volWid:
+            theta.append(curT)
+            curT += vw
+        bars = ax.bar(theta, height, width=volWid, bottom=0.0)
+        for c, bar in zip(color, bars):
+            bar.set_facecolor(cm.jet(c))
+            bar.set_alpha(0.5)
+            
+
+def runRandomTestCase(vCt=10):
+    """
+    run a random test case
+    vc is number of vaccines to try to fit
+    """
+    scores = []
+    vRecs = []
+    for i in xrange(vCt):
+        f,c,r = rDurs()
+        vRecs.append(makeVacRec("v%d"%i, f, c, r, random.uniform(0.5, 10.0)))
+
+    cbSizes = {'freezer':random.uniform(0,10.0), 
+               'cooler':random.uniform(0,10.0),
+               'roomtemperature':random.uniform(0,10.0)}
+
+    sim = _mockSim(vRecs, cbSizes)
+    sM = storagemodel.StorageModel(False)
+    pM = packagingmodel.SimplePackagingModel()
+    
+    tM = sim.typeManager
+    
+    truckType = tM.getTypeByName("truck1", sim=sim)
+    truck = truckType.createInstance()
+    truck.storageModel = sM
+    truck.packagingModel = pM
+    
+    vax = [tM.getTypeByName("v%d"%i) for i in xrange(vCt)]
+    vc = sim.shippables.getCollection()
+    for i in xrange(vCt):
+        vc += sim.shippables.getCollection([(vax[i], random.randrange(1000))])
+    
+    print vc
+#    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosPreferred(truck, vc, False)
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosSimple(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    scores.append(calcStats(truck, (fVC,cVC,wVC), vc))
+    
+
+    warehouse.DEBUG = False
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatios(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    if calcVols(vc, fVC) > cbSizes['freezer'] * 1000.1:
+        print("freezer alloc too high")
+        exit()
+    if calcVols(vc, cVC) > cbSizes['cooler'] * 1000.1:
+        print("cooler alloc too high")
+        exit()
+    if calcVols(vc, wVC) > cbSizes['roomtemperature'] * 1000.1:
+        print("roomtemperature alloc too high")
+        exit()
+    scores.append(calcStats(truck, (fVC,cVC,wVC), vc))
+
+    warehouse.DEBUG = False
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosPreferred(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    if calcVols(vc, fVC) > cbSizes['freezer'] * 1000.1:
+        print("freezer alloc too high")
+        exit()
+    if calcVols(vc, cVC) > cbSizes['cooler'] * 1000.1:
+        print("cooler alloc too high")
+        exit()
+    if calcVols(vc, wVC) > cbSizes['roomtemperature'] * 1000.1:
+        print("roomtemperature alloc too high")
+        exit()
+    scores.append(calcStats(truck, (fVC,cVC,wVC), vc))
+
+    return scores
+
+
+def chartRandomTestCase(vCt=10, caseNum=1):
+    """
+    run a random test case
+    vc is number of vaccines to try to fit
+    """
+
+    figs, axes = plt.subplots(nrows=3, ncols=3,subplot_kw=dict(polar=True))
+
+
+    vRecs = []
+    for i in xrange(vCt):
+        f,c,r = rDurs()
+        vRecs.append(makeVacRec("v%d"%i, f, c, r, random.uniform(0.5, 10.0)))
+
+    cbSizes = {'freezer':random.uniform(0,10.0), 
+               'cooler':random.uniform(0,10.0),
+               'roomtemperature':random.uniform(0,10.0)}
+
+    sim = _mockSim(vRecs, cbSizes)
+    sM = storagemodel.StorageModel(False)
+    pM = packagingmodel.SimplePackagingModel()
+    
+    tM = sim.typeManager
+    
+    truckType = tM.getTypeByName("truck1", sim=sim)
+    truck = truckType.createInstance()
+    truck.storageModel = sM
+    truck.packagingModel = pM
+    
+    vax = [tM.getTypeByName("v%d"%i) for i in xrange(vCt)]
+    vc = sim.shippables.getCollection()
+    for i in xrange(vCt):
+        vc += sim.shippables.getCollection([(vax[i], random.randrange(1000))])
+    
+    print vc
+#    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosPreferred(truck, vc, False)
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosSimple(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    chartCollection((fVC, cVC, wVC), vc, (axes[0,0], axes[0,1], axes[0,2]))
+    axes[0,0].set_title('simple freeze')
+    axes[0,1].set_title('simple cool')
+    axes[0,2].set_title('simple warm')
+
+    warehouse.DEBUG = False
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatios(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    if calcVols(vc, fVC) > cbSizes['freezer'] * 1000.1:
+        print("freezer alloc too high")
+        exit()
+    if calcVols(vc, cVC) > cbSizes['cooler'] * 1000.1:
+        print("cooler alloc too high")
+        exit()
+    if calcVols(vc, wVC) > cbSizes['roomtemperature'] * 1000.1:
+        print("roomtemperature alloc too high")
+        exit()
+ 
+    chartCollection((fVC, cVC, wVC), vc, (axes[1,0], axes[1,1], axes[1,2]))
+    axes[1,0].set_title('l1 freeze')
+    axes[1,1].set_title('l1 cool')
+    axes[1,2].set_title('l1 warm')
+
+    warehouse.DEBUG = False
+    fVC, cVC, wVC = warehouse.share3_calculateOwnerStorageFillRatiosPreferred(truck, vc, False)
+    print fVC
+    print cVC
+    print wVC
+    print cbSizes
+    print calcVols(vc, fVC)
+    print calcVols(vc, cVC)
+    print calcVols(vc, wVC)
+    if calcVols(vc, fVC) > cbSizes['freezer'] * 1000.1:
+        print("freezer alloc too high")
+        exit()
+    if calcVols(vc, cVC) > cbSizes['cooler'] * 1000.1:
+        print("cooler alloc too high")
+        exit()
+    if calcVols(vc, wVC) > cbSizes['roomtemperature'] * 1000.1:
+        print("roomtemperature alloc too high")
+        exit()
+    chartCollection((fVC, cVC, wVC), vc, (axes[2,0], axes[2,1], axes[2,2]))
+    axes[2,0].set_title('l2 freeze')
+    axes[2,1].set_title('l2 cool')
+    axes[2,2].set_title('l2 warm')
+    figs.savefig('blah')
+
+          
+#chartRandomTestCase()
+
+
+sumFr10=0
+sumFr21=0
+sumFr20=0
+betterCount = 0
+cases = 10000
+for i in xrange(cases):
+    scores = runRandomTestCase()
+    fr0 = scores[0]['facRate']
+    fr1 = scores[1]['facRate']
+    fr2 = scores[2]['facRate']
+
+    fr10 = fr0 / fr1
+    fr21 = fr1 / fr2
+    fr20 = fr0 / fr2
+
+    sumFr10 += fr10
+    sumFr21 += fr21
+    sumFr20 += fr20
+    if fr20 > 1.0:
+        betterCount += 1
+    
+    print "facRates"
+    print fr0, fr1, fr2
+    print "facCompares"
+    print fr10, fr21, fr20
+
+print sumFr10 / cases
+print sumFr21 / cases
+print sumFr20 / cases
+print float(betterCount) / cases
+
+
+
+
+exit()
 
 sim = _mockSim()
 sM = storagemodel.StorageModel(False)
@@ -278,6 +613,8 @@ v2 = tM.getTypeByName('VAC2')
 v3 = tM.getTypeByName('VAC3')
 vc = sim.shippables.getCollection([(v1, 5000), (v2, 3333), (v3, 10555)])
 print vc
+
+
 
 figs1, axes1 = plt.subplots(nrows=1, ncols=1)
 figs2, axes2 = plt.subplots(nrows=2, ncols=3)
@@ -342,4 +679,6 @@ axes1.set_aspect('equal')
 
 # figs1.tight_layout()
 # figs2.tight_layout()
-plt.show()
+#plt.show()
+figs1.savefig('blah1')
+figs2.savefig('blah2')
