57da8a7b2f87ca4a23dcd657b2be555bb99a6503
Shawn Brown <stbrown@psc.edu>
2016-06-21 14:29:01 -0400

    Added quotation marks to ensure that if there are paths with spaces, it doesn't mess things up.

--- dist/win32/misc/log_install_hermes.bat
+++ dist/win32/misc/log_install_hermes.bat
@@ -2,9 +2,9 @@
 setlocal
 set oldcurdir=%cd%
 cd /d %~dp0
-echo ------------------------------------------->>..\..\install_hermes.log
-echo Installing on %date% @ %time%:>>..\..\install_hermes.log
+echo ------------------------------------------->>"%AppData%\HERMES\install_hermes.log"
+echo Installing on %date% @ %time%:>> "%AppData%\HERMES\install_hermes.log"
 set PATH=%~dp0\..\..\python;%~dp0\..\..\python\Scripts;%PATH%
-python.exe install_hermes.py -n -a %AppData%\HERMES >>%AppData%\HERMES\install_hermes.log 2>&1
+python.exe install_hermes.py -n -a "%AppData%\HERMES" >>"%AppData%\HERMES\install_hermes.lo"g 2>&1
 cd /d %oldcurdir%
 endlocal
