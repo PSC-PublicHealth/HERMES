b23a71ec32a546e32a2ee7eab1b75dc1e4ee5b98
Shawn Brown <stbrown@psc.edu>
2016-08-24 14:55:11 -0400

    fixed a typo

--- dist/win32/misc/log_install_hermes.bat
+++ dist/win32/misc/log_install_hermes.bat
@@ -5,6 +5,6 @@ cd /d %~dp0
 echo ------------------------------------------->>"%AppData%\HERMES\install_hermes.log"
 echo Installing on %date% @ %time%:>> "%AppData%\HERMES\install_hermes.log"
 set PATH=%~dp0\..\..\python;%~dp0\..\..\python\Scripts;%PATH%
-python.exe install_hermes.py -n -a "%AppData%\HERMES" >>"%AppData%\HERMES\install_hermes.lo"g 2>&1
+python.exe install_hermes.py -n -a "%AppData%\HERMES" >>"%AppData%\HERMES\install_hermes.log" 2>&1
 cd /d %oldcurdir%
 endlocal
