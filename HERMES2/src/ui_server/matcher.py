5e471666cce031552071cc08414518533975d772
Shawn Brown <stbrown@psc.edu>
2017-08-14 11:22:08 -0400

    removed an extraneous import from matcher.py

--- HERMES2/src/ui_server/matcher.py
+++ HERMES2/src/ui_server/matcher.py
@@ -59,7 +59,7 @@ import model_population_demand_grid
 import factoryhooks
 import model_storage_inventory_grid
 import experimenthooks
-import databasehooks
+#import databasehooks
 
 ## If executing under mod_wsgi, we need to add the path to the source
 ## directory of this script.
